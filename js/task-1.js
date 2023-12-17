ac8c35bad0e541f84da02419489a8a2b02a896ea
VadymLys <144118540+VadymLys@users.noreply.github.com>
2023-12-17 19:26:10 +0100

    1

--- js/task-1.js
+++ js/task-1.js
@@ -1,17 +1,40 @@
-function makeTransaction(pricePerDroid, quantity, customerCredits){
-  let message;
-  const totalPrice = pricePerDroid * quantity;
-  const creditsLeft = customerCredits - totalPrice ;
-if (totalPrice <= customerCredits) {
-  message = `You ordered ${quantity} worth ${totalPrice} credits!`;
-} else {
-  message = `Insufficient funds!`;
-}
-  return message;
-}
+const getUserNames = users => users.map(user => user.name);
 
-console.log(makeTransaction(3000, 5, 23000)); 
-console.log(makeTransaction(1000, 3, 15000)); 
-console.log(makeTransaction(5000, 10, 8000)); 
-console.log(makeTransaction(2000, 8, 10000));
-console.log(makeTransaction(500, 10, 5000));
+ 
+console.log(getUserNames([
+    {
+        name: "Moore Hensley",
+        email: "moorehensley@indexia.com",
+        balance: 2811
+    },
+    {
+        name: "Sharlene Bush",
+        email: "sharlenebush@tubesys.com",
+        balance: 3821
+    },
+    {
+        name: "Ross Vazquez",
+        email: "rossvazquez@xinware.com",
+        balance: 3793
+    },
+    {
+        name: "Elma Head",
+        email: "elmahead@omatom.com",
+        balance: 2278
+    },
+    {
+        name: "Carey Barr",
+        email: "careybarr@nurali.com",
+        balance: 3951
+    },
+    {
+        name: "Blackburn Dotson",
+        email: "blackburndotson@furnigeer.com",
+        balance: 1498
+    },
+    {
+        name: "Sheree Anthony",
+        email: "shereeanthony@kog.com",
+        balance: 2764
+    },
+]));
--- js/task-4.js
+++ js/task-4.js
@@ -1,32 +1,44 @@
-function getShippingCost(country) {
-  let message;
+const getTotalBalanceByGender = (users, gender) => users.filter(user => user.gender === gender).reduce((acc, user) => acc + user.balance, 0);
 
+const allUsers = [
+    {
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811
+  },
+  {
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821
+  },
+  {
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793
+  },
+  {
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278
+  },
+  {
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951
+  },
+  {
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498
+  },
+  {
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764
+  }
+];
 
- switch (country) {
-   case "China": 
-   message = `Shipping to ${country} will cost 100 credits`;
-    break;
-   case "Chile":
-  message = `Shipping to ${country} will cost 250 credits`;
-     break;
-   case "Australia":
-     message = `Shipping to ${country} will cost 170 credits`;
-     break;
-   case "Jamaica":
-     message = `Shipping to ${country} will cost 120 credits`;
-     break;
-  default:
-  message = `Sorry, there is no delivery to your country`;
-  
- }
-  console.log(message);
+console.log(getTotalBalanceByGender(allUsers, "male")); // 12053
 
-  return message;
-}
+console.log(getTotalBalanceByGender(allUsers, "female")); // 8863
 
-getShippingCost("Australia");
-getShippingCost("Germany");
-getShippingCost("China");
-getShippingCost("Chile");
-getShippingCost("Jamaica");
-getShippingCost("Sweden");
